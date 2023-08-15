d950c3ed735621f4a8aa4cfcae7696f79af59396
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-16 02:34:20 +0300

    first commit

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,12 @@
+-- Task: Create the table first_table if it doesn't exist
+-- Query: CREATE TABLE IF NOT EXISTS first_table (
+--            id INT,
+--            name VARCHAR(256)
+--        );
+CREATE TABLE
+IF NOT EXISTS first_table
+(
+    id INT,
+    name VARCHAR
+(256)
+);
