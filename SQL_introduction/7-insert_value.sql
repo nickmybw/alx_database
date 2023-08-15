644b10d303f7c8384c983c3590b16af9e6191e9b
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-16 02:40:52 +0300

    first commit

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- Task: Insert a new row into the table first_table
+-- Query: INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
+INSERT INTO first_table
+  (id, name)
+VALUES
+  (89, 'Holberton School');
