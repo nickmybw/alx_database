20ec9e9c6eb8e597b92c3efa5da6353c96627029
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-16 03:18:32 +0300

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,8 @@
--- Script to list privileges of MySQL users
-
--- Connect to MySQL server
-mysql
--u root -p
+-- Task: My privileges!
 
 -- List privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS
+FOR 'user_0d_1'@'localhost';
 
 -- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
