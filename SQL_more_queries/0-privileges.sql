fb1904b52940692c9e66538725bfe244992b3ad8
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-16 03:09:27 +0300

    first commit

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,10 @@
+#
+Task:
+List all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+(in localhost)
+
+# List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+# List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
